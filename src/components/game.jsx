c3bdd6fc5f413ec3eecac343a268819ec3b0c2ea
Juan Rey <105689962+fernan2lopezkto@users.noreply.github.com>
2024-01-02 17:57:21 -0300

    add game.jsx

---
+++ src/components/game.jsx
@@ -0,0 +1,12 @@
+export default function Game() {
+    return (
+        <>
+        <div>
+          <h2>Pregunta</h2>
+          <div>
+            <p>Está es la pregunta</p>
+          </div>
+        </div>
+        </>
+    )
+}
