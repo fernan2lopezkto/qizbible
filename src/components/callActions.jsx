ea8edc7b9150ea0dd3513e2f226420c4fb564fe1
Juan Rey <105689962+fernan2lopezkto@users.noreply.github.com>
2024-01-02 18:11:59 -0300

    add callAction.jsx

---
+++ src/components/callActions.jsx
@@ -0,0 +1,15 @@
+export default function CallAction() {
+  return(
+    <>
+    <div>
+      <div>
+        <h1>Preguntas Bíblicas</h1>
+        <p>App de preguntas y respuestas bíblicas</p>
+      </div>
+      <div>
+        <img href="" />
+      </div>
+    </div>
+    </>
+  )
+}
