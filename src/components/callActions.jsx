c7f7b1de793cf5291d0a341d7d9110d29fff59ba
Juan <fernan2lopezkto@gmail.com>
2024-01-11 10:06:25 -0300

    finish call-actions component

--- src/components/callActions.jsx
+++ src/components/callActions.jsx
@@ -9,6 +9,7 @@ export default function CallAction() {
       <div className='text-container'>
         <h1>Preguntas Bíblicas</h1>
         <p>App de preguntas y respuestas bíblicas</p>
+        <button className='call-button'>Jugar</button>
       </div>
       <div className='container-logo'>
       <img src={pnge} className="App-logo" alt="logo" />
