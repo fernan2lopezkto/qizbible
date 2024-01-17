b64593429e21e92e5ce638114e8abf33b01f462e
Juan Rey <105689962+fernan2lopezkto@users.noreply.github.com>
2024-01-16 22:15:40 -0300

    Actualizar components

---
+++ src/components/navbar.jsx
@@ -0,0 +1,12 @@
+export default function NavBar() {
+  return (
+    <div ClassName="navbar-container">
+      <a>BQ<>
+      <div>
+        <a href="">Juego</a>
+        <a href="">Reglas</a>
+        <a href="">about</a>
+      </div>
+    </div>
+  )
+}
