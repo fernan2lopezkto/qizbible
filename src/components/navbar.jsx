2290050b084867c4026f9c029ce7fd18bc9bf0e7
Juan Rey <105689962+fernan2lopezkto@users.noreply.github.com>
2024-01-16 22:30:26 -0300

    Actualizar navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,6 @@
 export default function NavBar() {
   return (
-    <div ClassName="navbar-container" I'd="nav">
+    <div ClassName="navbar-container" id="nav">
       <a href="#nav" ClassName="nav-principal-link">BQ<>
       <div ClassName="nav-links-container">
         <a href="#game" ClassName="nav-other-link">Juego</a>
